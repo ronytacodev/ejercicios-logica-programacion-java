d14c62a56c86e0a26ada094f84a0898c0379ac93
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-05-04 20:16:39 -0500

    upload ejercicio tabla del 4 apartir del 5

---
+++ TablaDel4ApartirDel5/src/main/java/com/mycompany/tabladel4apartirdel5/TablaDel4ApartirDel5.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tabladel4apartirdel5;
+
+/**
+ *
+ * @author Administrador
+ */
+public class TablaDel4ApartirDel5 {
+
+    public static void main(String[] args) {
+                for (int i = 4; i <= 4; i++) {
+            for (int j = 5; j <= 10; j++) {
+                System.out.println(i+"*"+j+" = "+i*j);
+            }
+        }
+    }
+}
