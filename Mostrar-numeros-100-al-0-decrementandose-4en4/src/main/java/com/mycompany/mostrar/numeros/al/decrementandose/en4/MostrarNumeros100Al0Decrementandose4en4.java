ddfca8a8319dde28e66d55c2f4253a7329b60106
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-05-04 20:51:46 -0500

    upload ejercicio de mostrar números del 100 al 0 decrementándose de 4 en 4

---
+++ Mostrar-numeros-100-al-0-decrementandose-4en4/src/main/java/com/mycompany/mostrar/numeros/al/decrementandose/en4/MostrarNumeros100Al0Decrementandose4en4.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mostrar.numeros.al.decrementandose.en4;
+
+/**
+ *
+ * @author Administrador
+ */
+public class MostrarNumeros100Al0Decrementandose4en4 {
+
+    public static void main(String[] args) {
+        for (int i = 100; i >=0; i-=4) {
+            System.out.println(i);
+        }
+    }
+}
